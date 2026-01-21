5004c75e49d96c1050ec545dc845feb4c6e5f33a
emanueltms <emanuelk240@gmail.com>
2026-01-21 10:35:18 +0000

    Add project files.

---
+++ M17E_INTRO_12H/index.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace M17E_INTRO_12H
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            tb_x.Text = "0";
+            tb_y.Text = "0";
+        }
+
+        protected void bt_soma_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            x = int.Parse(tb_x.Text);
+            y = int.Parse(tb_y.Text);
+
+            lb_resultado.Text = (x + y).ToString();
+        }
+    }
+}
